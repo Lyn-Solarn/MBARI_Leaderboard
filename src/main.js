45dabda6b3d046ecfad92388af5cf50d46232a31
Lyn-Solarn <lalarson2003@gmail.com>
2025-11-19 20:48:43 -0800

    Started to set up the leaderboard table.

--- src/main.js
+++ src/main.js
@@ -2,4 +2,14 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+const vuetify = createVuetify({
+icons: { defaultSet: 'mdi', aliases, sets: { mdi } },
+})
+
+
+createApp(App).use(vuetify).mount('#app')
